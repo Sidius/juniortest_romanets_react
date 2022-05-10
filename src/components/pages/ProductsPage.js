d59fb5923acfd2b73cda83f1007d53c598fc5466
Romanets Pavel <pavelroma24@gmail.com>
2022-05-10 10:02:53 +0300

    up: updating products

--- src/components/pages/ProductsPage.js
+++ src/components/pages/ProductsPage.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import Header from "../header";
 import ProductServiceAPI from "../../services/ProductServiceAPI";
 import ProductList from "../productList";
 
@@ -11,43 +10,9 @@ export default class ProductsPage extends Component {
         document.title = "Product List";
     }
 
-    state = {
-        title: "Product List",
-        navButtons: [
-            {
-                title: "Add",
-                link: "/add-product",
-                id: null
-            },
-            {
-                title: "Mass delete",
-                id: "delete-product-btn",
-                onClick: this.deleteAllItems,
-            },
-        ],
-    }
-
-    deleteAllItems() {
-        const productServiceAPI = new ProductServiceAPI();
-        let checkboxes = document.querySelectorAll('.delete-checkbox:checked');
-
-        if (checkboxes.length !== 0) {
-            let response = true;
-            checkboxes.forEach((input) => {
-                let sku = input.value;
-                productServiceAPI.deleteProduct(sku).then((result) => {
-                    if (!response.response) {
-                        response = false;
-                    }
-                });
-            });
-        }
-    }
-
     render() {
         return(
             <React.Fragment>
-                <Header title={this.state.title} buttons={this.state.navButtons}/>
                 <ProductList getData={this.productServiceAPI.getAllProducts}/>
             </React.Fragment>
         );
--- src/components/productList/ProductList.js
+++ src/components/productList/ProductList.js
@@ -1,12 +1,50 @@
 import React, {Component} from "react";
 import ProductItem from "../productItem";
 import {Col, Container, Row, Spinner} from "react-bootstrap";
+import Header from "../header";
+import ProductServiceAPI from "../../services/ProductServiceAPI";
 
 export default class ProductList extends Component {
 
+
+
+    deleteAllItems = (event) => {
+        const productServiceAPI = new ProductServiceAPI();
+        let checkboxes = document.querySelectorAll('.delete-checkbox:checked');
+
+        if (checkboxes.length !== 0) {
+            let response = true;
+            checkboxes.forEach((input) => {
+                let sku = input.value;
+                productServiceAPI.deleteProduct(sku).then((result) => {
+                    if (!response.response) {
+                        response = false;
+                    }
+                });
+                this.setState({
+                    update: true,
+                });
+            });
+        }
+    }
+
     state = {
+        title: "Product List",
+        navButtons: [
+            {
+                title: "Add",
+                link: "/add-product",
+                id: null
+            },
+            {
+                title: "Mass delete",
+                id: "delete-product-btn",
+                onClick: this.deleteAllItems,
+                object: this,
+            },
+        ],
         itemList: null,
-        update: this.props.update,
+        update: false,
     }
 
     componentDidMount() {
@@ -20,7 +58,8 @@ export default class ProductList extends Component {
     }
 
     componentDidUpdate(prevProps, prevState, snapshot) {
-        if (this.props.update) {
+        console.log(this.state.update);
+        if (this.state.update) {
             const {getData} = this.props;
             getData().then((itemList) => {
                 this.setState({
@@ -53,11 +92,14 @@ export default class ProductList extends Component {
         const items = this.renderItems(this.state.itemList);
 
         return(
-            <Container fluid={true} style={{padding: '0 1.5em 2em',}}>
-                <Row md={4} className="g-4">
-                    {items}
-                </Row>
-            </Container>
+            <>
+                <Header title={this.state.title} buttons={this.state.navButtons}/>
+                <Container fluid={true} style={{padding: '0 1.5em 2em',}}>
+                    <Row md={4} className="g-4">
+                        {items}
+                    </Row>
+                </Container>
+            </>
         );
     }
 }
